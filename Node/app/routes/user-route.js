0c5207aafe87148077f125d31e00c27309526055
Cesar Azevedo <cesardeazevedo@outlook.com>
2015-05-03 04:14:23 -0300

    changed the userId parameter to email

--- Node/app/routes/user-route.js
+++ Node/app/routes/user-route.js
@@ -1,14 +1,14 @@
 var user = require('../controllers/user');
 
-module.exports = function(app)  {
+module.exports = function(app) {
     app.route('/users')
         .get(user.list)
         .post(user.create);
 
-    app.route('/users/:userId')
+    app.route('/users/:email')
         .get(user.read)
         .put(user.update)
         .delete(user.delete);
 
-    app.param('userId', user.userById);
+    app.param('email', user.userByEmail);
 };
