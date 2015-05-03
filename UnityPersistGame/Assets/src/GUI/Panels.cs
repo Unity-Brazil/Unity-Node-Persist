203e7fa5d94da8ea4c6d0fc1d33204ec2be7777c
Cesar Azevedo <cesardeazevedo@outlook.com>
2015-05-03 04:32:02 -0300

    release panel control

---
+++ UnityPersistGame/Assets/src/GUI/Panels.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Panels : MonoBehaviour
+{
+
+    public Image PanelEmail;
+    public Image PanelScore;
+    public Image PanelGrid;
+
+    public static Panels Instance { get; private set; }
+
+    public void Awake()
+    {
+        Instance = this;
+
+        //Initialize with email registration panel
+        this.ShowEmail();
+    }
+
+    public void ShowEmail()
+    {
+        DisablePanels();
+        PanelEmail.gameObject.SetActive(true);
+    }
+
+    public void ShowScore()
+    {
+        DisablePanels();
+        PanelScore.gameObject.SetActive(true);
+    }
+
+    public void ShowGrid()
+    {
+        DisablePanels();
+        PanelGrid.gameObject.SetActive(true);
+    }
+
+    private void DisablePanels()
+    {
+        PanelEmail.gameObject.SetActive(false);
+        PanelScore.gameObject.SetActive(false);
+        PanelGrid.gameObject.SetActive(false);
+    }
+}
